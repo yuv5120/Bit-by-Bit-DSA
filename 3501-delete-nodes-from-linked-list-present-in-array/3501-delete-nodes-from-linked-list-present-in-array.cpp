47bc12b985a7080cf35d9ed6e7c288254f54719b
Yuvraj Singh Gour <124714578+yuv5120@users.noreply.github.com>
2025-11-01 23:08:41 +0530

    Time: 93 ms (76.23%), Space: 263.2 MB (86.67%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
         unordered_set<int> mpp(nums.begin(), nums.end());
-
         while (head && mpp.count(head->val))
             head = head->next;
 
